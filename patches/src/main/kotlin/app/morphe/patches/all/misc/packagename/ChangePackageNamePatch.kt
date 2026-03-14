63a1484b236aacdd0ac3c8e2783b17f2ddf5ba33
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-03-14 05:29:52 +0100

    chore: Add temporary runtime check for old versions of Morphe

--- patches/src/main/kotlin/app/morphe/patches/all/misc/packagename/ChangePackageNamePatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/all/misc/packagename/ChangePackageNamePatch.kt
@@ -170,6 +170,18 @@ val changePackageNamePatch = resourcePatch(
 
     dependsOn(bytecodePatch {
         execute {
+            // TODO: Eventually remove this check. Early versions of Morphe Manager
+            //       may not auto update if GitHub non auth API blocks the user ip.
+            try {
+                //noinspection CheckResult
+                ResourcePatchContext::class.java.getDeclaredField("packageMetadata")
+            } catch (e: NoSuchFieldException) {
+                throw RuntimeException(
+                    "Your Morphe app is outdated. Please manually update Morphe " +
+                            "by downloading from https://Morphe.software"
+                )
+            }
+
             when (val originalPackageName = packageMetadata.packageName) {
                 PACKAGE_NAME_REDDIT -> {
                     applyGetPackageName(
