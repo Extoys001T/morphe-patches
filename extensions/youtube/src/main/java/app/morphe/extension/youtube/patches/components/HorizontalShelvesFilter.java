decd49ad54b4296315d923fb96e0c8db103042d9
inotia00 <108592928+inotia00@users.noreply.github.com>
2026-03-19 20:59:39 +0900

    fix(YouTube - Hide layout components): `Hide horizontal shelves` hides the `Video description` components

--- extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/components/HorizontalShelvesFilter.java
+++ extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/components/HorizontalShelvesFilter.java
@@ -69,13 +69,18 @@ final class HorizontalShelvesFilter extends Filter {
         );
     }
 
+    private boolean isPlayerOrDescription() {
+        return EngagementPanel.isDescription()
+                || PlayerType.getCurrent().isMaximizedOrFullscreen()
+                || isActionBarVisible.get()
+                || ShortsPlayerState.isOpen();
+    }
+
     private boolean hideShelves(ContextInterface contextInterface) {
-        if (!Settings.HIDE_HORIZONTAL_SHELVES.get()) {
+        if (!Settings.HIDE_HORIZONTAL_SHELVES.get() || isPlayerOrDescription()) {
             return false;
         }
         return contextInterface.isHomeFeedOrRelatedVideo()
-                || PlayerType.getCurrent().isMaximizedOrFullscreen()
-                || isActionBarVisible.get()
                 || NavigationBar.isSearchBarActive()
                 || NavigationBar.isBackButtonVisible()
                 || NavigationButton.getSelectedNavigationButton() != NavigationButton.LIBRARY;
@@ -97,7 +102,7 @@ final class HorizontalShelvesFilter extends Filter {
             return true;
         }
         if (descriptionBuffers.check(buffer).isFiltered()) {
-            return EngagementPanel.isDescription() || PlayerType.getCurrent().isMaximizedOrFullscreen() || isActionBarVisible.get() || ShortsPlayerState.isOpen();
+            return isPlayerOrDescription();
         }
         return hideShelves(contextInterface);
     }
